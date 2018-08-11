57297c4216c19e5b1d3cdf503fc8270e417f23aa
Nicolas Pare <nicolaspare313@gmail.com>
2018-08-11 14:33:36 -0400

    Initial Commit

---
+++ VideoApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VideoApp
+//
+//  Created by Nicolas Paré on 18-08-11.
+//  Copyright © 2018 Nicolas Paré. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
